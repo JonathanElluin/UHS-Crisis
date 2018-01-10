7450ece75e43ae8ea58c03e1d8d28a1d69aa1162
Maengast <adntouch@gmail.com>
2018-01-10 16:03:28 +0100

    create

---
+++ UHS-SHOOTER/Assets/Script/HUD/HealthManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthManager : MonoBehaviour {
+
+    public int MaxHealth;
+    int LifePoints;
+    public bool isAlive = true;
+    public Scrollbar LifeBar;
+
+    public GameObject prefabConfetis;
+
+	// Use this for initialization
+	void Start () {
+        LifePoints = MaxHealth;
+	}
+	
+
+    public void TakeDammage(int _dammage)
+    {
+        LifePoints -= _dammage;
+        LifeBar.size = (float)LifePoints * 1 / MaxHealth;
+
+        // Si le personnage n'a plus de points de vie
+        if (LifePoints <= 0)
+        {
+            isAlive = false;
+            Destroy(gameObject);
+            Instantiate(prefabConfetis, transform.position, Quaternion.identity);
+        }
+    }
+}
