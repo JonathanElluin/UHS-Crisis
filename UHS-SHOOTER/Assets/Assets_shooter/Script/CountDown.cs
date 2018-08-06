695b00611be66d0b1afaed7f740a0a06242ae754
Dj00lls <j.elluin@gmail.com>
2018-08-06 19:57:13 +0200

    push du code pret pour export

---
+++ UHS-SHOOTER/Assets/Assets_shooter/Script/CountDown.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountDown : MonoBehaviour
+{
+
+    public int timer;
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(StartTimer());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public IEnumerator StartTimer()
+    {
+        while (timer > 0)
+        {
+            yield return new WaitForSeconds(1.0f);
+            timer--;
+        }
+        Destroy(this.gameObject);
+    }
+}
